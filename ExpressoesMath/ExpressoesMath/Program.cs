e309c35eeff32c5bf085dee4a12b58db708a213c
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-12 17:58:46 -0300

    Expressões Matemáticas 01

---
+++ ExpressoesMath/ExpressoesMath/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 3.0;
+            double y = 4.0;
+            double z = -5.0;
+            double A, B, C;
+
+            A = Math.Sqrt(x);
+            B = Math.Sqrt(y);
+            C = Math.Sqrt(25.0);
+            Console.WriteLine("Raiz quadrada de " + x + " = " + A.ToString("F2"));
+            Console.WriteLine("Raiz quadrada de " + y + " = " + B);
+            Console.WriteLine("Raiz quadrada de 25 - " + C);
+
+            Console.WriteLine("---------------------------------------------------");
+
+            A = Math.Pow(x, y);
+            B = Math.Pow(x, 2.0);
+            C = Math.Pow(5.0, 2.0);
+            Console.WriteLine(x + " elevado a " + y + " = " + A);
+            Console.WriteLine(x + " elevado ao quadrado = " + B);
+            Console.WriteLine("5 elevado ao quadrado = " + C);
+
+            Console.WriteLine("---------------------------------------------------");
+
+            A = Math.Abs(y);
+            B = Math.Abs(z);
+            Console.WriteLine("Valor absoluto de " + y + " = " + A);
+            Console.WriteLine("Valor absoluto de " + z + " = " + B);
+
+
+            Console.ReadLine();
+        }   
+    }
+}
