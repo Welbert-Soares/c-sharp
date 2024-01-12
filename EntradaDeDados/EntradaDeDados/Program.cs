b358bb7873095d3f68d45f39e9d15f492da07127
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-12 15:30:53 -0300

    Entrada de dados

---
+++ EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string cor1 = Console.ReadLine();
+            string cor2 = Console.ReadLine();
+            string cor3 = Console.ReadLine();
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(cor1);
+            Console.WriteLine(cor2);
+            Console.WriteLine(cor3);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+        }   
+
+    }
+}
--- EntradaDeDados/EntradaDeDados/obj/Debug/net8.0/EntradaDeDados.AssemblyInfo.cs
+++ EntradaDeDados/EntradaDeDados/obj/Debug/net8.0/EntradaDeDados.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Expressoes")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1408645190c7f00713bdf1de2e65d5f47e15599")]
-[assembly: System.Reflection.AssemblyProductAttribute("Expressoes")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Expressoes")]
+[assembly: System.Reflection.AssemblyProductAttribute("EntradaDeDados")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EntradaDeDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
