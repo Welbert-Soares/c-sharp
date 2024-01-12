f842df7d28b94304f7633ac5518015a32a71240a
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-12 17:07:25 -0300

    Entrada de dados 2

---
+++ EntradaDeDados2/EntradaDeDados2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            string[] vet = Console.ReadLine().Split(' ');
+            string name = vet[0];
+            char sex = char.Parse(vet[1]);
+            int age = int.Parse(vet[2]);
+            double height = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+
+
+            Console.WriteLine("Você digitou: " + n1);
+            Console.WriteLine("Você digitou: " + ch);
+            Console.WriteLine("Você digitou: " + d.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(vet[0]);
+            Console.WriteLine(vet[1]);
+            Console.WriteLine(vet[2]);
+            Console.WriteLine(vet[3], CultureInfo.InvariantCulture);
+        }
+    }
+}
