e3a01df32399f581c68a2c102e2b8a3b8fe540e4
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-15 18:37:52 -0300

    Exercício 002

---
+++ Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Security.AccessControl;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, h, metro, area, valor;
+
+            Console.WriteLine("Qual a largura do terreno(base): ");
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Qual a profundidade do terreno(altura): ");
+            h = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Qual o valor do metro quadrado? ");
+            metro = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            area = b * h;
+
+            valor = metro * area;
+
+            Console.WriteLine("A área do terreno é de " + area.ToString("F2", CultureInfo.InvariantCulture) + " metros quadrados, e o valor do terreno é de R$" + valor.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+
+        }
+    }
+}
