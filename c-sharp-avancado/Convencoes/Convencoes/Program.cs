35ff95993b756603c624079d9d290db83ae8cd80
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-01-17 16:23:27 -0300

    camelCase c#

---
+++ c-sharp-avancado/Convencoes/Convencoes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Convencoes
+{
+    class ContaBancaria
+    {
+        public string Titular { get; set; }
+        private double _saldo;
+
+        public void Deposito(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+    }    
+}
---
+++ c-sharp-avancado/Course/Course/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            bool completo = false;
+            char genero = 'F';
+            char letra = '\u0041';
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green";
+            object obj1 = "Alex Brown";
+            object obj2 = 4.5f;
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+        }
+    }
+}
